a46dfdf023a1b0653ca519b1676ac6299ce4a6af
Hanson Nguyen <hansonhn369@gmail.com>
2024-01-29 23:57:47 -0800

    Camera setup

--- src/app/post/index.tsx
+++ src/app/post/index.tsx
@@ -3,6 +3,7 @@ import { Camera, CameraType, FlashMode, PermissionStatus } from "expo-camera";
 import { useEffect, useRef, useState } from "react";
 import * as MediaLibrary from "expo-media-library";
 import { Alert, Image, StyleSheet } from "react-native";
+import { router } from "expo-router";
 
 export default function PostPage() {
 	const [cameraPermissionStatus, setCameraPermissionStatus] = useState(
@@ -14,21 +15,22 @@ export default function PostPage() {
 	const cameraRef = useRef<Camera | null>(null);
 
 	useEffect(() => {
-		(async () => {
+		const askPermissions = async () => {
 			MediaLibrary.requestPermissionsAsync();
 			const cameraStatus = await Camera.requestCameraPermissionsAsync();
 			setCameraPermissionStatus(cameraStatus.status);
-		})();
-	}, []);
 
-	// if (cameraPermissionStatus !== PermissionStatus.GRANTED) {
-	// 	Alert.alert(
-	// 		"No Permission",
-	// 		"You need camera permissions to continue.",
-	// 		[{ text: "OK", onPress: () => console.log("OK Pressed") }]
-	// 	);
-	// 	return <></>;
-	// }
+			if (cameraStatus.status !== PermissionStatus.GRANTED) {
+				Alert.alert(
+					"No Permission",
+					"You need camera permissions to continue.",
+					[{ text: "OK", onPress: () => router.back() }]
+				);
+				return <></>;
+			}
+		};
+		askPermissions();
+	}, []);
 
 	const takePicture = async () => {
 		if (cameraRef.current) {
@@ -42,6 +44,12 @@ export default function PostPage() {
 		}
 	};
 
+	const toggleFlashMode = () => {
+		setFlash((current) =>
+			current === FlashMode.off ? FlashMode.on : FlashMode.off
+		);
+	};
+
 	const toggleCameraType = () => {
 		setType((current) =>
 			current === CameraType.back ? CameraType.front : CameraType.back
@@ -51,23 +59,35 @@ export default function PostPage() {
 	return (
 		<View style={styles.container}>
 			{!image ? (
-				<Camera
-					style={styles.camera}
-					ref={cameraRef}
-					type={type}
-					flashMode={flash}
-				></Camera>
+				<>
+					<Camera
+						style={styles.camera}
+						ref={cameraRef}
+						type={type}
+						flashMode={flash}
+					>
+						<View style={styles.captureSettingsContainer}>
+							<Text onPress={() => router.back()}>Close</Text>
+							<Text onPress={toggleFlashMode}>Flash {flash}</Text>
+							<Text>Next</Text>
+						</View>
+						<Pressable
+							style={styles.takePictureButton}
+							onPress={takePicture}
+						/>
+					</Camera>
+					<View style={styles.bottomContainer}>
+						<Text>View</Text>
+						<View style={styles.captureTypeContainer}>
+							<Text>Normal</Text>
+							<Text>Video</Text>
+						</View>
+						<Text onPress={toggleCameraType}>Flip</Text>
+					</View>
+				</>
 			) : (
 				<Image source={{ uri: image }} style={styles.camera} />
 			)}
-			<View style={styles.bottomContainer}>
-				<Pressable
-					style={styles.takePictureButton}
-					onPress={takePicture}
-				>
-					<Text>Take a Picture</Text>
-				</Pressable>
-			</View>
 		</View>
 	);
 }
@@ -80,14 +100,35 @@ const styles = StyleSheet.create({
 	},
 	camera: {
 		flex: 1,
+		justifyContent: "space-between",
 		width: "100%",
 		borderRadius: 20,
 	},
-	bottomContainer: {
-		height: "10%",
-	},
 	takePictureButton: {
-		borderRadius: 100,
-		width: "10%",
+		borderRadius: 40,
+		width: 80,
+		height: 80,
+		marginBottom: "8%",
+		marginLeft: "auto",
+		marginRight: "auto",
+	},
+	captureSettingsContainer: {
+		backgroundColor: "transparent",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-around",
+		marginTop: "8%",
+	},
+	bottomContainer: {
+		width: "100%",
+		paddingVertical: 48,
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-around",
+	},
+	captureTypeContainer: {
+		display: "flex",
+		flexDirection: "row",
+		gap: 16,
 	},
 });
